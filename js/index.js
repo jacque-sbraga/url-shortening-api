c238e081d275719dd19258a429fc0594a3af24cb
Jacqueline dos Santos Braga <jaquelinesantosbraga@gmail.com>
2022-03-25 23:47:11 -0300

    completed header and organized the components into separate css files

--- js/index.js
+++ js/index.js
@@ -1,12 +1,16 @@
 const bars = document.querySelector(".navbar-mobile");
 
-const handleMenu = () => {
+const handleMenu = (event) => {
     const navbar = document.querySelector(".navbar");
-    const isVisible = navbar.classList.contains("showMenu");
-
-    !isVisible?
-    navbar.classList.add("showMenu")
-    :navbar.classList.remove("showMenu");
+    const isActive = navbar.classList.contains("active");
+   
+    if(event.type === "touchstart"){
+        event.preventDefault();
+    }
+    
+    event.currentTarget.setAttribute("aria-expanded", isActive? false : true)
+    navbar.classList.toggle("active");
 }
 
 bars.addEventListener("click", handleMenu);
+bars.addEventListener("touchstart", handleMenu, { passive: false })
