3b109c06717d1b8dd0e9ca9a91f4f455a46ebe17
Jacqueline dos Santos Braga <jaquelinesantosbraga@gmail.com>
2022-04-13 20:11:38 -0300

    pending to integrate with the API, finish the footer and other things.

--- js/index.js
+++ js/index.js
@@ -1,5 +1,9 @@
 const bars = document.querySelector(".navbar-mobile");
-
+// https://clipboardjs.com/
+// https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Interact_with_the_clipboard
+// https://blog.erikfigueiredo.com.br/area-de-transferencia-copiar-e-colar-com-javascript-dica-rapida/#:~:text=Ctrl%2BC%20%7C%20Ctrl%2BV%20com%20JS!
+// https://www.delftstack.com/pt/howto/javascript/javascript-copy-to-clipboard/
+// https://www.youtube.com/watch?v=K7r2-hBydBE
 const handleMenu = (event) => {
     const navbar = document.querySelector(".navbar");
     const isActive = navbar.classList.contains("active");
